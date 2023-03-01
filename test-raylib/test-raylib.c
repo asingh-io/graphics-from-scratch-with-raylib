9536d1a5350c99ea897dc73e8d0e6a189dea2402
Anand Singh <mail.tickiot@gmail.com>
2023-03-01 11:20:49 -0800

    Updated with the DrawSprial function

--- test-raylib/test-raylib.c
+++ test-raylib/test-raylib.c
@@ -4,12 +4,25 @@
 const int screenWidth = 800;
 const int screenHeight = 800;
 
-void put_pixel(int x , int y, Color color)
+void PutPixel(int x , int y, Color color)
 {
     int sx = screenWidth /2 + x;
     int sy = screenHeight /2 - y;
     DrawPixel(sx, sy, color);
 
+}
+
+void DrawSpiral()
+{
+    float angle = 0;
+    float radius = 1;
+    while(radius < screenWidth/2){
+        float y = radius * sin(angle);
+        float x = radius * cos(angle);
+        PutPixel((int)x, (int)y, RED);
+        radius += 0.005;
+        angle += 3.14159f * .0005f;
+    }
 }
 
 int main(void)
@@ -19,18 +32,10 @@ int main(void)
     while (!WindowShouldClose())
     {
         BeginDrawing();
-            ClearBackground(RAYWHITE);
-//            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-        float angle = 0;
-        float radius = 1;
-        while(radius < screenWidth/2){
-            float y = radius * sin(angle);
-            float x = radius * cos(angle);
-            put_pixel((int)x, (int)y, RED);
-            radius += 0.005;
-            angle += 3.14159f * .0005f;
+        ClearBackground(RAYWHITE);
 
-        }
+        DrawSpiral();
+
         EndDrawing();
     }
 
